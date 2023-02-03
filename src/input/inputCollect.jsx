5b4deba650adb8b9f7c0c3cadbd11e157eb89958
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-02-03 12:57:54 +0600

    added some funcationality in jsx file

--- src/input/inputCollect.jsx
+++ src/input/inputCollect.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import './inputCollect.css';
 
 function InputCollect(){
+    // let number1, number2;
+    const [num1, setNum1] = useState(0);
+    const firstNumber = (event)=>{
+        setNum1(Number(event.target.value));
+    }
+
+    const [num2, setNum2] = useState(0);
+    const secondNumber = (event)=>{
+        setNum2(Number(event.target.value));
+    }
+
+    function addNumbers(){
+        let ans = num1+num2;
+        console.log(ans);
+    }
+
     return (
         <div className="input__container">
             <h1>Congratulations everyone</h1>
             <div className="input__field">
-                <input placeholder='First Number' type="number" />
-                <input placeholder='Second Number' type="number" />
+                <input onChange={firstNumber} placeholder='First Number' type="number" />
+                <input onChange={secondNumber} placeholder='Second Number' type="number" />
             </div>
             <div className="operator">
-                <button>+</button>
+                <button onClick={addNumbers()}>+</button>
                 <button>-</button>
                 <button>*</button>
                 <button>/</button>
